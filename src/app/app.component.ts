567fbd12b28ea998cfcd7d43d3da90d298110944
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-04-15 13:24:57 +0200

    Merge pull request #1 from zindien-hub/feature/improve-navbar-ui
    fix: improve navbar layout and fix rendering issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [
+    CommonModule,
     RouterOutlet,
     RouterLink
   ],
